e01884de551077228189b6a5f174e444082d291f
Kevin <kevin.whetten@emmersion.ai>
2021-07-20 15:24:46 -0600

    Copy Emmersion.Http and reference it as project instead of NuGet package
    Use '/scripts/Copy-Emmersion.Http.sh' to update

---
+++ scripts/Copy-Emmersion.Http.sh
@@ -0,0 +1,3 @@
+cp -r ../../Emmersion.Http/Emmersion.Http ../Emmersion.Http
+cp -r ../../Emmersion.Http/Emmersion.Http.UnitTests ../Emmersion.Http
+cp -r ../../Emmersion.Http/Emmersion.Http.IntegrationTests ../Emmersion.Http
